1b02f4e2eadfa6813788326e2c83d906a5a0111f
Zux12 <158550944+Zux12@users.noreply.github.com>
2025-09-10 14:50:43 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,6 @@
 // server/db.js
 const { MongoClient, GridFSBucket, ObjectId } = require('mongodb');
 
-// Accept common env var names
 const uri =
   process.env.MONGO_URI ||
   process.env.MONGODB_URI ||
@@ -13,18 +12,16 @@ if (!uri) {
   throw new Error("MONGO_URI missing");
 }
 
-
 let _client, _db, _bucket;
 
 async function connect() {
   if (_db) return _db;
   _client = new MongoClient(uri);
   await _client.connect();
-  _db = _client.db("Asianloop"); // your DB name
+  _db = _client.db("Asianloop");
   _bucket = new GridFSBucket(_db, { bucketName: "commonFiles" });
   return _db;
 }
-
 function db() { if (!_db) throw new Error("DB not connected yet"); return _db; }
 function bucket() { if (!_bucket) throw new Error("Bucket not ready"); return _bucket; }
 function oid(id) { return new ObjectId(id); }
